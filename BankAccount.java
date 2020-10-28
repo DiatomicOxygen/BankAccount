a6178c03b488c1aee1567c29580edf74d55ec62c
DiatomicOxygen <eoo20@stuy.edu>
2020-10-28 14:02:18 -0400

    Created constructor for BankAccount class

--- BankAccount.java
+++ BankAccount.java
@@ -3,4 +3,9 @@ public class BankAccount {
     private double balance = 0.0;
     private int accountID = 0;
     private String password = "";
+
+    public BankAccount(int newID, String newPassword) {
+      accountID = newID;
+      password = newPassword;
+    }
 }
--- Customer.java
+++ Customer.java
@@ -1,5 +1,6 @@
 public class Customer {
   public static void main(String[] args) {
+    BankAccount B1 = new BankAccount(1, "12345");
 
   }
 }
